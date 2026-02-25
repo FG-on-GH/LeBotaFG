73fe4f768ff2631d04bbcd7ea911d57bc24d00f0
FG-on-GH <francois-gabriel.sourbe@ensea.fr>
2026-02-25 23:22:15 +0100

    Mise en place des Cogs pour permettre le multifichier

--- bot.py
+++ bot.py
@@ -11,17 +11,24 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Création de l'objet bot avec le préfixe qui sera utilisé pour les commandes et le permissions accordées
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('/'), intents=intents)
+# On crée une classe spécifique pour le bot héritant de la classe Discord de base
+class LeBotaFG(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or('/'), intents=intents)
+
+    # setup_hook appelée automatiquement juste avant que le bot se connecte
+    async def setup_hook(self):
+        # Parcourt des fichiers du dossier cogs
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                # Si le fichier est un python on charge l'extension
+                await self.load_extension(f'cogs.{filename[:-3]}')
+                print(f"Cog chargé : {filename}")
+
+bot = LeBotaFG()
 
 @bot.event
-async def on_ready():   # Evènement automatiquement déclenché quand le bot est connecté et prêt
+async def on_ready():       # Evènement automatiquement déclenché quand le bot est connecté et prêt
     print(f'Connecté en tant que {bot.user}')
-
-
-@bot.command()
-async def salut(ctx):
-    await ctx.send(f'Salut {ctx.author.name} !')
-# ctx.send : envoie un message dans le même salon que le contexte de la commande
 
 bot.run(TOKEN)
---
+++ cogs/salut.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Salut(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Dans un Cog, on utilise @commands.command() au lieu de @bot.command()
+    # et la fonction prend "self" comme premier argument !
+    @commands.command()
+    async def salut(self, ctx):
+        await ctx.send(f'Salut {ctx.author.name} !')
+
+# Obligatoire à la fin de chaque fichier Cog
+# Permet au fichier principal d'ajouter ce module au bot
+async def setup(bot):
+    await bot.add_cog(Salut(bot))
